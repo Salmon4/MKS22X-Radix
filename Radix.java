53d01e41c92bdc829a6b6ffc5bbe52d34914b2c5
Brandon <bchen00@stuy.edu>
2019-04-01 10:17:59 -0400

    worked on Radix

---
+++ Radix.java
@@ -0,0 +1,21 @@
+public class Radix{
+  public static void radixsort(int[]data){
+    int max = ;
+  }
+
+  private static void radixsorthelper(int[]data, int num){
+    MyLinkedList bucket0 = new MyLinkedList();
+    MyLinkedList bucket1 = new MyLinkedList();
+    MyLinkedList bucket2 = new MyLinkedList();
+    MyLinkedList bucket3 = new MyLinkedList();
+    MyLinkedList bucket4 = new MyLinkedList();
+    MyLinkedList bucket5 = new MyLinkedList();
+    MyLinkedList bucket6 = new MyLinkedList();
+    MyLinkedList bucket7 = new MyLinkedList();
+    MyLinkedList bucket8 = new MyLinkedList();
+    MyLinkedList bucket9 = new MyLinkedList();
+    for (int i = 0; int i < data.length; i++){
+      if (data[i] % )
+    }
+  }
+}
