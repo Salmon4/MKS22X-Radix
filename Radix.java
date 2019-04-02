cd1000615cd71f7bf7437b1d1b459acb7388a917
Brandon <bchen00@stuy.edu>
2019-04-02 09:56:53 -0400

    properly made the bucket and add values to it

--- Radix.java
+++ Radix.java
@@ -14,19 +14,39 @@ public class Radix{
   }
 
   private static void deciSort(int[]data, int num){
-    MyLinkedList<Integer> zeros = new MyLinkedList();
-    MyLinkedList<Integer> ones = new MyLinkedList();
-    MyLinkedList<Integer> twos = new MyLinkedList();
-    MyLinkedList<Integer> threes = new MyLinkedList();
-    MyLinkedList<Integer> fours = new MyLinkedList();
-    MyLinkedList<Integer> fives = new MyLinkedList();
-    MyLinkedList<Integer> sixs = new MyLinkedList();
-    MyLinkedList<Integer> sevens = new MyLinkedList();
-    MyLinkedList<Integer> eights = new MyLinkedList();
-    MyLinkedList<Integer> nines = new MyLinkedList();
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
     for (int i = 0; i < data.length;i++){
       int currentDigit = (data[i]/num)%10;
-      if (){}
+      if (currentDigit == 0){
+        buckets[0].add(data[i]);
+      }
+      if (currentDigit == 1){
+        buckets[1].add(data[i]);
+      }
+      if (currentDigit == 2){
+        buckets[2].add(data[i]);
+      }
+      if (currentDigit == 3){
+        buckets[3].add(data[i]);
+      }
+      if (currentDigit == 4){
+        buckets[4].add(data[i]);
+      }
+      if (currentDigit == 5){
+        buckets[5].add(data[i]);
+      }
+      if (currentDigit == 6){
+        buckets[6].add(data[i]);
+      }
+      if (currentDigit == 7){
+        buckets[7].add(data[i]);
+      }
+      if (currentDigit == 8){
+        buckets[8].add(data[i]);
+      }
+      if (currentDigit == 9){
+        buckets[9].add(data[i]);
+      }
     }
   }
 }
