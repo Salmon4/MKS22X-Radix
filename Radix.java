0bbe91d9f26b3eb3d4add21a7a404a1c34b79f01
Brandon Chen <bchen00@stuy.edu>
2019-04-03 21:52:36 -0400

    got rid of sort ones. wrote for loop going through all the digit places

--- Radix.java
+++ Radix.java
@@ -1,5 +1,47 @@
-public class Radix{
-  public static void radixsort(int[] data){
+import java.util.Arrays;
+
+public class Radix {
+  @SuppressWarnings("unchecked")
+  public static void radixsort(int[] data) {
+    if (data.length == 0) return;
+    int max = data[0];
+    for (int x = 0; x < data.length; x++) {
+      if (data[x] > max) max = data[x];
+    }
+    int maxDigits = String.valueOf(max).length();
+    //@SuppressWarnings("unchecked")
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
+    for (int i = 0; i < buckets.length; i++) {
+      buckets[i] = new MyLinkedList();
+    }
+    MyLinkedList data2 = new MyLinkedList();
+
+    for (int i = 0; i < maxDigits; i++) {
+      for (int j = 0; j < data.length; j++) {
+        int ans = (data[j]/(int)Math.pow(10,i))%10;
+      if (data[j] > 0) {
+        buckets[10+Math.abs(ans)].add(data[j]);
+      } else {
+        buckets[9-Math.abs(ans)].add(data[j]);
+        }
+      }
+      for (int k = 0; k < buckets.length; k++) {
+        data2.extend(buckets[k]);
+      }
+      for (int l = 0; i < data.length; l++) {
+        data[l] = (int)data2.removeFront();
+      }
+    }
+  }
+
+
+  public static void main(String[] args){
+
+  }
+}
+
+
+/**
     int max = 0;
     for (int i = 0; i < data.length; i++){
       if (data[i] > max){
@@ -16,10 +58,10 @@ public class Radix{
     for (int i = 0; i < data.length; i++){
       int ones = data[i] % 10;
       if (data[i] < 0){
-        buckets[].add() //positive
+        buckets[9 + ones].add(data[i]); //positive
       }
       else{
-        buckets[].add() //negative
+        buckets[10 + ones].add(data[i]); //negative
       }
     }
 
@@ -28,9 +70,34 @@ public class Radix{
       data2.extend(buckets[i]);
     }
 
+    Node currentNode = data2.getNthNode(1);
     for (int currentDigit = 1; currentDigit < maxdigits; currentDigit++){
-      
+      for (int i2 = 0; i2 < data2.size(); i2++){
+        //System.out.println("" + (int)currentNode.getData());
+        int value = (int)currentNode.getData();
+        int place = (value / power(data[currentDigit])) % 10;
+        if (value < 0){
+          buckets[9 + place].add(value);
+        }
+        else{
+          buckets[10 + place].add(value);
+        }
+        currentNode = currentNode.next();
+      }
+      for (int i = 0; i < buckets.length; i++){
+        data2.extend(buckets[i]);
+      }
     }
+    int j = 0;
+    currentNode = data2.getNthNode(0);
+    for (int i = 0; i < data2.size(); i++){
+      data[j] = (int)currentNode.getData();
+      j++;
+      currentNode = currentNode.next();
+    }
+  }
+
+
     //for (int i = 0; i < data.length; i++){
     //  data2.add(data[i]);
     //}
@@ -38,7 +105,7 @@ public class Radix{
       //deciSort(data2,deciPlace);
     //}
 
-  }
+
 
 /**
   private static void deciSort(MyLinkedList data, int num){
@@ -90,4 +157,3 @@ public class Radix{
     data = bucket[0];
   }
   **/
-}
